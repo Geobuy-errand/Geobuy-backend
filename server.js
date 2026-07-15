c40dd4929a30a07a266c88c2a510a927b2f36ee5
Promise Johnson <chiemelapromise30@gmail.com>
2026-07-16 00:16:27 +0100

    update

--- server.js
+++ server.js
@@ -48,6 +48,10 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res)=>{
+  return res.send('Welcome to GEOBUY Errands API');
+})
 app.use('/api', limiter);
 
 // Routes
